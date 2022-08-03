3b8372271d2ddc51d4ce5cb8aff119e89691309f
Kotaro <swdevstudio@gmail.com>
2022-08-03 19:58:50 +0300

    Добавил nuxt декоратор, убрал автоимпорт.

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <Tutorial />
+  <div>
+    <NuxtTutorial></NuxtTutorial>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import NuxtTutorial from "~/components/Tutorial.vue";
 
 export default Vue.extend({
   name: 'IndexPage',
+  components: {NuxtTutorial},
 })
 </script>
