78bcc3dbed1f4b194ef36c4891c0082fdd591252
Kotaro <swdevstudio@gmail.com>
2022-08-04 19:26:18 +0300

    Сделал пример роутинга и ssr

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,8 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import NuxtTutorial from "~/components/Tutorial.vue";
 
 export default Vue.extend({
   name: 'IndexPage',
-  components: {NuxtTutorial},
 })
 </script>
